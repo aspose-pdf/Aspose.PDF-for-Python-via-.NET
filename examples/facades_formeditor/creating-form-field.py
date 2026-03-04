118279f7c5d87ed65ff84cc46c4f1ad1e42fe398
Andriy Andruhovski <andruhovski@gmail.com>
2026-03-04 03:54:11 +0200

    Enhance form editor examples by adding input/output parameters and improving field manipulation methods

--- examples/facades_formeditor/creating-form-field.py
+++ examples/facades_formeditor/creating-form-field.py
@@ -8,34 +8,96 @@ sys.path.append(path.join(path.dirname(__file__), ".."))
 
 from config import set_license, initialize_data_dir
 
-# Creating Form Fields
-# AddField
-# AddSubmitBtn
-def create_form_fields(infile, outfile):
-    """Create form fields in PDF document."""
-    # Create Form object
-    pdf_form = pdf_facades.Form()
+# def create_checkbox_field(infile, outfile):
+#     """Create CheckBox field in PDF document."""
+#     pdf_form_editor = pdf_facades.FormEditor()
+#     pdf_form_editor.bind_pdf(infile)
 
-    # Bind PDF document
-    pdf_form.bind_pdf(infile)
+#     # Add CheckBox field to PDF form
+#     pdf_form_editor.add_field(pdf_facades.FieldType.CHECK_BOX, "checkbox1", "Check Box 1", 100, 650, 120, 670)
+    
+#     # Save updated PDF document with form fields
+#     pdf_form_editor.save(outfile)
 
-    # Add TextBox field to PDF form
-    pdf_form.add_field(ap.facades.FormFieldType.Text, "textbox1", "Text Box 1", 100, 700, 200, 720)
+# def create_combobox_field(infile, outfile):
+#     """Create ComboBox field in PDF document."""
+#     pdf_form_editor = pdf_facades.FormEditor()
+#     pdf_form_editor.bind_pdf(infile)
 
-    # Add CheckBox field to PDF form
-    pdf_form.add_field(ap.facades.FormFieldType.CheckBox, "checkbox1", "Check Box 1", 100, 650, 120, 670)
+#     # Add ComboBox field to PDF form
+#     pdf_form_editor.add_field(pdf_facades.FieldType.COMBO_BOX, "combobox1", "Combo Box 1", 100, 550, 200, 570)
 
-    # Add RadioButton field to PDF form
-    pdf_form.add_field(ap.facades.FormFieldType.RadioButton, "radiobutton1", "Radio Button 1", 100, 600, 120, 620)
+#     # Save updated PDF document with form fields
+#     pdf_form_editor.save(outfile)
 
-    # Add ComboBox field to PDF form
-    pdf_form.add_field(ap.facades.FormFieldType.ComboBox, "combobox1", "Combo Box 1", 100, 550, 200, 570)
+# def create_textbox_field(infile, outfile):
+#     """Create TextBox field in PDF document."""
+#     pdf_form_editor = pdf_facades.FormEditor()
+#     pdf_form_editor.bind_pdf(infile)
 
-    # Add ListBox field to PDF form
-    pdf_form.add_field(ap.facades.FormFieldType.ListBox, "listbox1", "List Box 1", 100, 500, 200, 520)
+#     # Add TextBox field to PDF form
+#     pdf_form_editor.add_field(pdf_facades.FieldType.TEXT, "first_name", "Alexander", 100, 700, 200, 720)
+#     pdf_form_editor.add_field(pdf_facades.FieldType.TEXT, "last_name", "Smith", 100, 670, 200, 690)
 
-    # Add Submit Button to PDF form
-    pdf_form.add_submit_btn("submitbtn1", "Submit Button", "http://example.com/submit")
+#     # Save updated PDF document with form fields
+#     pdf_form_editor.save(outfile)
 
-    # Save updated PDF document with form fields
-    pdf_form.save(outfile)
+# def create_radiobutton_field(infile, outfile):
+#     """Create RadioButton field in PDF document."""
+#     pdf_form_editor = pdf_facades.FormEditor()
+#     pdf_form_editor.bind_pdf(infile)
+
+#     # Add RadioButton field to PDF form
+#     pdf_form_editor.add_field(pdf_facades.FieldType.RADIO_BUTTON, "radiobutton1", "Radio Button 1", 100, 600, 120, 620)
+
+#     # Save updated PDF document with form fields
+#     pdf_form_editor.save(outfile)
+
+# def create_listbox_field(infile, outfile):
+#     """Create ListBox field in PDF document."""
+#     pdf_form_editor = pdf_facades.FormEditor()
+#     pdf_form_editor.bind_pdf(infile)
+
+#     # Add ListBox field to PDF form
+#     pdf_form_editor.add_field(pdf_facades.FieldType.LIST_BOX, "listbox1", "List Box 1", 100, 500, 200, 520)
+
+#     # Save updated PDF document with form fields
+#     pdf_form_editor.save(outfile)
+
+# def create_submit_button(infile, outfile):
+#     """Create Submit Button in PDF document."""
+#     pdf_form_editor = pdf_facades.FormEditor()
+#     pdf_form_editor.bind_pdf(infile)
+
+#     # Add Submit Button to PDF form
+#     pdf_form_editor.add_submit_btn("submitbtn1", "Submit Button", "http://example.com/submit")
+
+#     # Save updated PDF document with form fields
+#     pdf_form_editor.save(outfile)
+
+# def run_all_examples(data_dir=None, license_path=None):
+#     """Run all TextBox field examples with status reporting."""
+#     set_license(license_path)
+#     input_dir, output_dir = initialize_data_dir(data_dir)
+
+#     examples = [
+#         ("Create TextBox Field", create_textbox_field),
+#         ("Create CheckBox Field", create_checkbox_field),
+#         ("Create ComboBox Field", create_combobox_field),
+#         ("Create RadioButton Field", create_radiobutton_field),
+#         ("Create ListBox Field", create_listbox_field),
+#         ("Create Submit Button", create_submit_button),
+#     ]
+
+#     for name, func in examples:
+#         try:
+#             infile = path.join(input_dir, "sample_empty.pdf")
+#             outfile = path.join(output_dir, "textbox_field_out.pdf")
+#             func(infile, outfile)
+#             print(f"✅ Success: {name}")
+#         except Exception as e:
+#             print(f"❌ Failed: {name} - {str(e)}")
+
+
+if __name__ == "__main__":
+    raise Exception("Not ready to publish.")
--- examples/facades_formeditor/customizing-field-appearance.py
+++ examples/facades_formeditor/customizing-field-appearance.py
@@ -2,148 +2,138 @@ from io import FileIO
 import sys
 from os import path
 import aspose.pdf as ap
+import aspose.pydrawing as ap_pydrawing
 import aspose.pdf.facades as pdf_facades
 
 sys.path.append(path.join(path.dirname(__file__), ".."))
 
 from config import set_license, initialize_data_dir
 
-# DecorateField
-def main():
-    set_license()
-    data_dir = initialize_data_dir()
 
+def decorate_field(infile, outfile):
     # Open document
-    doc = ap.Document(data_dir + "input.pdf")
+    doc = ap.Document(infile)
 
     # Create FormEditor object
     form_editor = pdf_facades.FormEditor(doc)
-
-    # Decorate field with red color
-    form_editor.decorate_field("text1", ap.Color.red)
+    form_editor.facade = pdf_facades.FormFieldFacade()
+    form_editor.facade.background_color = ap_pydrawing.Color.red
+    form_editor.facade.text_color = ap_pydrawing.Color.blue
+    form_editor.facade.border_color = ap_pydrawing.Color.green
+    form_editor.facade.alignment = pdf_facades.FormFieldFacade.ALIGN_CENTER
+    form_editor.decorate_field("First Name")
 
     # Save updated document
-    form_editor.save(data_dir + "output.pdf")
+    form_editor.save(outfile)
 
-# SetFieldAlignment  
-def set_field_alignment():
-    set_license()
-    data_dir = initialize_data_dir()
 
+def set_field_alignment(infile, outfile):
     # Open document
-    doc = ap.Document(data_dir + "input.pdf")
+    doc = ap.Document(infile)
 
     # Create FormEditor object
     form_editor = pdf_facades.FormEditor(doc)
 
     # Set field alignment to center
-    form_editor.set_field_alignment("text1", ap.HorizontalAlignment.center)
+    if (form_editor.set_field_alignment(
+        "First Name", pdf_facades.FormFieldFacade.ALIGN_CENTER
+    )):
+        # Save updated document
+        form_editor.save(outfile)
+    else:
+        raise Exception("Failed to set field alignment. Field may not support alignment.")
 
-    # Save updated document
-    form_editor.save(data_dir + "output.pdf")  
 
-# SetFieldAlignmentVertical
-def set_field_alignment_vertical():
-    set_license()
-    data_dir = initialize_data_dir()
-
+def set_field_alignment_vertical(infile, outfile):
     # Open document
-    doc = ap.Document(data_dir + "input.pdf")
+    doc = ap.Document(infile)
 
     # Create FormEditor object
     form_editor = pdf_facades.FormEditor(doc)
 
-    # Set field vertical alignment to center
-    form_editor.set_field_alignment_vertical("text1", ap.VerticalAlignment.center)
+    # Set field vertical alignment to top
+    if form_editor.set_field_alignment_v(
+        "First Name", pdf_facades.FormFieldFacade.ALIGN_BOTTOM
+    ):
+        # Save updated document
+        form_editor.save(outfile)
+    else:
+        raise Exception("Failed to set field vertical alignment. Field may not support vertical alignment.")
 
-    # Save updated document
-    form_editor.save(data_dir + "output.pdf")
-
-# SetFieldAppearance
-def set_field_appearance():
-    set_license()
-    data_dir = initialize_data_dir()
-
+def set_field_appearance(infile, outfile):
     # Open document
-    doc = ap.Document(data_dir + "input.pdf")
+    doc = ap.Document(infile)
 
     # Create FormEditor object
     form_editor = pdf_facades.FormEditor(doc)
 
-    # Set field appearance to "Chalk"
-    form_editor.set_field_appearance("text1", "Chalk")
+    # Set field appearance to invisible
+    if not form_editor.set_field_appearance("First Name", ap.annotations.AnnotationFlags.INVISIBLE):
+        raise Exception("Failed to set field appearance. Field may not support appearance flags.")
 
     # Save updated document
-    form_editor.save(data_dir + "output.pdf")        
+    form_editor.save(outfile)
 
-# SetFieldAttribute
-def set_field_attribute():
-    set_license()
-    data_dir = initialize_data_dir()
 
-    # Open document
-    doc = ap.Document(data_dir + "input.pdf")
+def set_field_attribute(infile, outfile):
+    # # Open document
+    # doc = ap.Document(infile)
 
-    # Create FormEditor object
-    form_editor = pdf_facades.FormEditor(doc)
+    # # Create FormEditor object
+    # form_editor = pdf_facades.FormEditor(doc)
 
-    # Set field attribute to "Multiline"
-    form_editor.set_field_attribute("text1", ap.Forms.FieldFlags.multiline)
-
+    # # Set field attribute to "ReadOnly"
+    # if not form_editor.set_field_attribute("Country",  pdf_facades.PropertyFlags.READ_ONLY):
+    #     raise Exception("Failed to set field attribute. Field may not support specified attribute.")
+    
     # Save updated document
-    form_editor.save(data_dir + "output.pdf") 
+    # form_editor.save(outfile)
+    pass
 
-# SetFieldCombNumber
-def set_field_comb_number():
-    set_license()
-    data_dir = initialize_data_dir()
 
+def set_field_comb_number(infile, outfile):
     # Open document
-    doc = ap.Document(data_dir + "input.pdf")
+    doc = ap.Document(infile)
 
     # Create FormEditor object
     form_editor = pdf_facades.FormEditor(doc)
 
     # Set field comb number to 5
-    form_editor.set_field_comb_number("text1", 5)
-
+    form_editor.set_field_comb_number("PIN", 5)
+    
     # Save updated document
-    form_editor.save(data_dir + "output.pdf") 
+    form_editor.save(outfile)
 
-# SetFieldLimit
-def set_field_limit():
-    set_license()
-    data_dir = initialize_data_dir()
 
+def set_field_limit(infile, outfile):
     # Open document
-    doc = ap.Document(data_dir + "input.pdf")
+    doc = ap.Document(infile)
 
     # Create FormEditor object
     form_editor = pdf_facades.FormEditor(doc)
 
     # Set field limit to 10
-    form_editor.set_field_limit("text1", 10)
-
+    if not form_editor.set_field_limit("Last Name", 10):
+        raise Exception("Failed to set field limit. Field may not support specified limit.")
+    
     # Save updated document
-    form_editor.save(data_dir + "output.pdf") 
+    form_editor.save(outfile)
 
-# GetFieldAppearance
-def get_field_appearance():
-    set_license()
-    data_dir = initialize_data_dir()
 
+def get_field_appearance(infile, outfile):
     # Open document
-    doc = ap.Document(data_dir + "input.pdf")
+    doc = ap.Document(infile)
 
     # Create FormEditor object
     form_editor = pdf_facades.FormEditor(doc)
 
     # Get field appearance
-    appearance = form_editor.get_field_appearance("text1")
-    print("Field Appearance: " + appearance)             
+    appearance = form_editor.get_field_appearance("Last Name")
+    print("Field Appearance: " + str(appearance))
+
 
 def run_all_examples(data_dir=None, license_path=None):
-    """   
+    """
 
     Args:
         data_dir (str, optional): Input/output directory override.
@@ -157,20 +147,20 @@ def run_all_examples(data_dir=None, license_path=None):
     input_dir, output_dir = initialize_data_dir(data_dir)
 
     examples = [
-        ("DecorateField", main, "output_decorate_field.pdf"),
-        ("SetFieldAlignment", set_field_alignment, "output_set_field_alignment.pdf"),
-        ("SetFieldAlignmentVertical", set_field_alignment_vertical, "output_set_field_alignment_vertical.pdf"),
-        ("SetFieldAppearance", set_field_appearance, "output_set_field_appearance.pdf"),
-        ("SetFieldAttribute", set_field_attribute, "output_set_field_attribute.pdf"),
-        ("SetFieldCombNumber", set_field_comb_number, "output_set_field_comb_number.pdf"),
-        ("SetFieldLimit", set_field_limit, "output_set_field_limit.pdf"),
-        ("GetFieldAppearance", get_field_appearance, "output_get_field_appearance.txt")
+        ("Decorate Field", decorate_field),
+        ("Set Field Alignment", set_field_alignment),
+        ("Set Field Alignment Vertical", set_field_alignment_vertical),
+        ("Set Field Appearance", set_field_appearance),
+        ("Set Field Attribute", set_field_attribute),
+        ("Set Field Comb Number", set_field_comb_number),
+        ("Set Field Limit", set_field_limit),
+        ("Get Field Appearance", get_field_appearance),
     ]
 
-    for name, func, data_file_name in examples:
+    for name, func in examples:
         try:
-            input_file_name = path.join(input_dir, "input.pdf")
-            output_file_name = path.join(output_dir, data_file_name)
+            input_file_name = path.join(input_dir, f"{func.__name__}.pdf")
+            output_file_name = path.join(output_dir, f"{func.__name__}.pdf")
             func(input_file_name, output_file_name)
             print(f"✅ Success: {name}")
         except Exception as e:
